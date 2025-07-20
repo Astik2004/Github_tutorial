ba059bdf52a8b5ffb011de115b6560c1ad66f14b
Astik <astikyadav4@gmail.com>
2025-07-20 22:29:03 +0530

    This is the code for addition and Substraction

---
+++ CPP/Add.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2, num3, sum;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    cout << "Enter third number: ";
+    cin >> num3;
+
+    sum = num1 + num2 + num3;
+
+    cout << "The sum is: " << sum << endl;
+
+    return 0;
+}
